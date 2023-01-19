b86826960b102acf691717710aa6030db794b805
Juan Campillo <juancawool@outlook.com>
2023-01-19 12:09:53 -0500

    Retrieving api-gateway config files from server config and change logger type to info

--- src/main/java/com/decamplearning/gateway/config/RequestTraceFilter.java
+++ src/main/java/com/decamplearning/gateway/config/RequestTraceFilter.java
@@ -25,12 +25,12 @@ public class RequestTraceFilter implements GlobalFilter {
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         HttpHeaders requestHeaders = exchange.getRequest().getHeaders();
         if (isCorrelationIdPresent(requestHeaders)) {
-            logger.debug("java-training-correlation-id found in tracing filter: {}. ",
+            logger.info("java-training-correlation-id found in tracing filter: {}. ",
                     filterUtility.getCorrelationId(requestHeaders));
         } else {
             String correlationID = generateCorrelationId();
             exchange = filterUtility.setCorrelationId(exchange, correlationID);
-            logger.debug("java-training-correlation-id generated in tracing filter: {}.", correlationID);
+            logger.info("java-training-correlation-id generated in tracing filter: {}.", correlationID);
         }
         return chain.filter(exchange);
     }
--- src/main/java/com/decamplearning/gateway/config/ResponseTraceFilter.java
+++ src/main/java/com/decamplearning/gateway/config/ResponseTraceFilter.java
@@ -23,7 +23,7 @@ public class ResponseTraceFilter {
             return chain.filter(exchange).then(Mono.fromRunnable(() -> {
                 HttpHeaders requestHeaders = exchange.getRequest().getHeaders();
                 String correlationId = filterUtility.getCorrelationId(requestHeaders);
-                logger.debug("Updated the correlation id to the outbound headers. {}", correlationId);
+                logger.info("Updated the correlation id to the outbound headers. {}", correlationId);
                 exchange.getResponse().getHeaders().add(filterUtility.CORRELATION_ID, correlationId);
             }));
         };
