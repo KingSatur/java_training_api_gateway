a633ffbc82124254582f352cb72ea4a8a1c8e432
Juan Campillo <juancawool@outlook.com>
2023-03-14 21:27:40 -0500

    feat: setting up cors for api gateway

--- src/main/java/com/decamplearning/gateway/config/SecurityConfig.java
+++ src/main/java/com/decamplearning/gateway/config/SecurityConfig.java
@@ -1,21 +1,34 @@
 package com.decamplearning.gateway.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
+
     @Bean
     public SecurityWebFilterChain sprinSecurityWebFilterChain(ServerHttpSecurity serverHttpSecurity){
         serverHttpSecurity.csrf().disable()
-                .authorizeExchange(authorizeExchangeSpec ->
+                .cors().configurationSource(request-> {
+                    CorsConfiguration configuration = new CorsConfiguration();
+                    configuration.addAllowedOriginPattern("http://localhost:4200");
+                    configuration.setAllowCredentials(true);
+                    configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+                    configuration.setAllowedHeaders(List.of("*"));
+                    return configuration;
+                }).and().authorizeExchange(authorizeExchangeSpec ->
                         authorizeExchangeSpec
-                                .pathMatchers("/eureka/**")
+                                .pathMatchers("/**")
                                 .permitAll()
                                 .anyExchange()
                                 .authenticated()
